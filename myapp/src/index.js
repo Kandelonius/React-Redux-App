e110ba0defa490764d5ee983e16863a70d996a66
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-13 13:16:35 -0700

    have initial set up done and my api selected.

---
+++ myapp/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import * as serviceWorker from './serviceWorker';
+
+const store = createStore(applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+    </Provider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
+
+
